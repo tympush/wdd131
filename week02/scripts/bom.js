8e84b2fa3f162eec9649b5502b4cc9c1855d3c01
tympush <tymur.pushnoy@gmail.com>
2024-04-29 17:32:03 +0200

    week 2 Event Handling

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,10 +2,24 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
+button.addEventListener('click', function() {
+    if (input.value.trim() !== ''){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
-li.append(deleteButton);
-list.append(li);
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+    }
+
+    input.focus();
+});
