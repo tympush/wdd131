a7a814c4640f5e70f850d2f5f1e44bc2fd1b4408
tympush <tymur.pushnoy@gmail.com>
2024-05-28 19:16:24 +0200

    changed header to have alternate source for smaller images, have lazyload and be all populated dynamically through js instead of having 20 picture elements with mostly the same data in the html

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,3 +1,30 @@
+const imageContainer = document.querySelector('.changing-image-container');
+
+for (let i = 1; i <= 20; i++) {
+    const picture = document.createElement('picture');
+
+    const source = document.createElement('source');
+    source.srcset = `images/banner${i}.webp`;
+    source.media = '(min-width: 960px)';
+    source.width = 1920;
+    source.height = 500;
+
+    const img = document.createElement('img');
+    img.className = 'changing-image';
+    img.src = `images/banner${i}-smol.webp`;
+    img.alt = `Changing Image ${i}`;
+    img.width = 960;
+    img.height = 250;
+    img.loading = 'lazy';
+
+    picture.appendChild(source);
+    picture.appendChild(img);
+
+    imageContainer.appendChild(picture);
+}
+
+
+
 const images = document.querySelectorAll('.changing-image');
 
 let currentIndex = Math.floor(Math.random() * images.length);
