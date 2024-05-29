85edc5d3234115889875fd597ffe072453e9bbc4
tympush <tymur.pushnoy@gmail.com>
2024-05-29 20:51:53 +0200

    made the info page dropdown work

--- project/scripts/info-faction-changer.js
+++ project/scripts/info-faction-changer.js
@@ -5,16 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
     function updateVisibility() {
         const screenWidth = window.innerWidth;
       
-        // Check if a faction is currently selected
         const selectedFactionId = factionSelect.value;
         const selectedFactionElement = document.getElementById(selectedFactionId);
       
         if (screenWidth < 700) {
             infoRows.forEach(row => {
-                if (!selectedFactionElement || row.id !== selectedFactionId) {
-                    row.setAttribute('hidden', true);
-                }
+                row.setAttribute('hidden', true);
             });
+            handleFactionSelection();
         } else {
             infoRows.forEach(row => {
                 row.removeAttribute('hidden');
@@ -22,30 +20,23 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
     
-
     function handleFactionSelection() {
         const selectedFactionId = factionSelect.value;
-        console.log("Selected faction ID:", selectedFactionId);
-        // Hide all infoRow elements
-        console.log("Hiding all infoRow elements");
+    
+        infoRows.forEach(row => row.removeAttribute('hidden'));
         infoRows.forEach(row => row.setAttribute('hidden', true));
-        // Show the selected faction if it exists
-        const selectedFactionElement = document.getElementById(selectedFactionId);
-        console.log("Selected faction element:", selectedFactionElement);
+    
+        const selectedFactionElement = document.getElementById(selectedFactionId).parentElement;
+
         if (selectedFactionElement) {
-            console.log("Removing 'hidden' attribute from selected faction element");
             selectedFactionElement.removeAttribute('hidden');
-            console.log("Selected faction element shown:", selectedFactionElement);
-        } else {
-            console.log("Selected faction element not found.");
         }
     }
     
-    
-
     factionSelect.addEventListener("change", handleFactionSelection);
 
-    // Trigger visibility update on load and on window resize
     updateVisibility();
+    handleFactionSelection();
+
     window.addEventListener("resize", updateVisibility);
 });
